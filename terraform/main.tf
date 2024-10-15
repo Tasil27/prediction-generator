14e22fe2dac3d24471b952865f4539b2d50a3657
Tasil27 <chrissy.eick27@googlemail.com>
2024-10-15 10:59:28 +0200

    Adding the main project terraform template.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,21 @@
-resource "aws_lambda_function" "lambda_function" {
-    function_name = var.function_name
-    role = var.lambda_role_arn
-    handler = var.handler
-    runtime = var.runtime
+provider "aws" {
+    region = "eu-central-1"
+}
 
-    filename = var.lambda_zip_path
-    source_code_hash = filebase64sha256(var.lambda_zip_path)
+module "prediction_function_execution_role" {
+    source = "./modules/iam"
+    role_name = "prediction_generator_function_execution_role"
+    policy_name = "AWSLambdaBasicExecutionRole"
+}
 
-    environment {
-      variables = var.lambda_environment_vars
-    }
+module "predition_generator_lambda" {
+    source = "./modules/lambda"
+    function_name = "prediction_generator_function"
+    runtime = "nodejs20.x"
+    handler = "index.handler"
+    lambda_role_arn = module.prediction_function_execution_role.lambda_execution_role_arn
+    lambda_zip_path = "${path.module}/prediction-generator-lambda.zip"
+    lambda_environment_vars = {
+      API_TOKEN = "test_value"
+    }  
 }
